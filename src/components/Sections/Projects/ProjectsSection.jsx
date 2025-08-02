632578e4d11ba42a61366ccbf52b7380bf6c8c8a
Carlos de Leon <admin@koxland.net>
2025-08-01 21:10:23 -0600

    Update project section layout and badge styles
    Improved the visual appearance of the Projects section by adjusting spacing, adding a 'View More' button, and refining the gradient divider. Updated the completed state badge to use darker text and background for better contrast. Enhanced the Spanish translation for the Sacbe project title for clarity.

--- src/components/Sections/Projects/ProjectsSection.jsx
+++ src/components/Sections/Projects/ProjectsSection.jsx
@@ -3,18 +3,21 @@ import ProjectCard from '../../Cards/ProjectCard/ProjectCard';
 
 function ProjectsSection() {
   return (
-    <div className="flex w-full flex-col gap-10">
+    <div className="flex w-full flex-col">
       <div>
         <h2 className="mb-2 text-center text-4xl font-bold text-[#c2c6da]">
           Projects
         </h2>
-        <div className="mx-auto h-1 w-24 rounded-full bg-gradient-to-r from-orange-400 to-yellow-400"></div>
+        <div className="mx-auto mb-10 h-1 w-24 rounded-full bg-gradient-to-r from-orange-400 to-yellow-400"></div>
       </div>
-      <div className="flex flex-col gap-4 lg:flex-row">
+      <div className="mb-6 flex flex-col gap-10 lg:flex-row">
         <ProjectCard projectKey="fws" />
         <ProjectCard projectKey="sacbe" />
         <ProjectCard projectKey="couponGenerator" />
       </div>
+      <button className="hover:flash hidden w-fit cursor-pointer self-center rounded-[9999px] bg-white px-2 py-1.5 font-medium text-black transition-all duration-300 ease-in-out hover:bg-orange-400/80 hover:text-white lg:block">
+        View More
+      </button>
     </div>
   );
 }
